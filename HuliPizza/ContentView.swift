12e69385f559443108db9cc4e710a78f6415e9fd
francisco <franciscolira408@gmail.com>
2023-04-09 12:28:29 -0500

    Suing labels to update UI In the application

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -20,15 +20,20 @@ struct ContentView: View {
                 Text("Huli Pizza Company")
                     .background()
             }
-            
+            Label{
+                Text(59.99, format: .currency(code: "USD"))
+            }
+        icon: {
             Image(systemName: orders.isEmpty ? "cart" : "cart.circle.fill")
-            
+        }
             HStack {
+//                Label("Cart", systemImage: "cart")
                 Text("Order Pizza")
                     .font(.title)
                 Spacer()
                 Text(19.90, format: .currency(code: "USD"))
             }
+            
             ScrollView{
                 ForEach(orders, id: \.self ){order in
                     HStack(alignment: .firstTextBaseline){
